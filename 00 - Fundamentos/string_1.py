33bb6cb30dfde46a53c6990d6aedec36b50cec78
luizns <luns.wm@hotmail.com>
2025-08-31 16:21:00 -0300

    feat: add fundamentals of variables, data types, loops, condicionals, and logical operators

---
+++ 00 - Fundamentos/string_1.py
@@ -0,0 +1,19 @@
+nome = "lUiZ"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "  Olá mundo!    "
+
+print(texto + ".")
+print(texto.strip() + ".")
+print(texto.rstrip() + ".")
+print(texto.lstrip() + ".")
+
+menu = "Java"
+
+print("####" + menu + "####")
+print(menu.center(14))
+print(menu.center(14, "*"))
+print("-".join(menu))
